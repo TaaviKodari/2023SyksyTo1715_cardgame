8993203943de7c6ac1d0f6bfd24a098ed8bcb747
Your Name <you@example.com>
2023-09-07 18:14:06 +0300

    luotiin kortti komponentti

---
+++ 2023SyksyTo1715_card_game/src/App.jsx
@@ -0,0 +1,10 @@
+import Card from './components/Card'
+
+export default function App(){
+  return(
+    <>
+      <h1>Hello world!</h1>
+      <Card/>
+    </>
+  );
+}
---
+++ 2023SyksyTo1715_card_game/src/components/Card.jsx
@@ -0,0 +1,6 @@
+
+export default function Card(){
+    return(
+        <h2>MIAU :DDDDDD</h2>
+    );
+}
