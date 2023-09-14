4dc9e566b31b63a24f50b7544cf2cc3dce6324cf
Your Name <you@example.com>
2023-09-14 18:15:02 +0300

    css tehtiin

--- 2023SyksyTo1715_card_game/src/App.jsx
+++ 2023SyksyTo1715_card_game/src/App.jsx
@@ -1,4 +1,5 @@
 import Card from './components/Card'
+import './App.css'
 
 export default function App(){
   return(
--- 2023SyksyTo1715_card_game/src/components/Card.jsx
+++ 2023SyksyTo1715_card_game/src/components/Card.jsx
@@ -1,6 +1,18 @@
 
 export default function Card(){
     return(
-        <h2>MIAU :DDDDDD</h2>
+       <div className="card">
+            <img src="http://placekitten.com/120/100"/>
+            <ul>
+                <li>
+                    <span>Cuteness</span>
+                    <span>10</span>
+                </li>
+                <li>
+                    <span>Speed</span>
+                    <span>10</span>
+                </li>
+            </ul>
+       </div>
     );
 }
