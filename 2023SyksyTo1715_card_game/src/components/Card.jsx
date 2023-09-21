576f64b0f3ddaff421a25429c7fb080643df861b
Your Name <you@example.com>
2023-09-21 18:13:16 +0300

    css loppuun

--- 2023SyksyTo1715_card_game/src/components/Card.jsx
+++ 2023SyksyTo1715_card_game/src/components/Card.jsx
@@ -3,15 +3,11 @@ export default function Card(){
     return(
        <div className="card">
             <img src="http://placekitten.com/120/100"/>
-            <ul>
-                <li>
+            <ul className="stat-list">
+                <li className="stat-list-item">
                     <span>Cuteness</span>
                     <span>10</span>
-                </li>
-                <li>
-                    <span>Speed</span>
-                    <span>10</span>
-                </li>
+                </li> 
             </ul>
        </div>
     );
