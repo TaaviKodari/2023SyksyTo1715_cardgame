2ae27d38cd3c90c74a88b6392f042b03b5e0a16f
Your Name <you@example.com>
2023-10-26 18:11:01 +0300

    randomisointi

--- 2023SyksyTo1715_card_game/src/App.jsx
+++ 2023SyksyTo1715_card_game/src/App.jsx
@@ -1,9 +1,12 @@
 import Card from './components/Card'
 import './App.css'
+import { useState } from 'react'
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min +1)+min);
 
 const playerCard = {
   image: 'http://placekitten.com/120/100',
-  stats: [{name:'Cuteness', value: 9},
+  stats: [{name:'Cuteness', value: getRandomInt(1, 999)},
           {name:'Speed', value: 50}
                                     ]
 }
@@ -17,21 +20,21 @@ const opponentCard = {
 
 export default function App(){
 
+  const[result, setResult] = useState('');
+
   function compareCards(){
     const playerStat = playerCard.stats[0];
     const opponentStat = opponentCard.stats[0];
 
-    let result = "";
-
     if( playerStat.value === opponentStat.value){
-      result = "Draw";
+     setResult("Draw");
     }
     else if(playerStat.value > opponentStat.value){
-      result = "Win";
+      setResult("Win");
     }else{
-      result = "Loss";
+      setResult("Loss");
     }
-    console.log(result);
+
   }
 
   return(
@@ -39,7 +42,10 @@ export default function App(){
       <h1>Hello world!</h1>
       <div className='game'>
         <Card card={playerCard}/>
-        <button onClick={compareCards} type="button">Play</button>
+        <div className='center-area'>
+          <p>{result || 'Press the button'}</p>
+          <button onClick={compareCards} type="button">Play</button>
+        </div>
         <Card card={opponentCard}/>
       </div>
     </>
