b7be664efd36d2ddf8a1b530724d4125770e2d00
Your Name <you@example.com>
2023-12-07 18:16:12 +0200

    nextRound kesken

--- 2023SyksyTo1715_card_game/src/App.jsx
+++ 2023SyksyTo1715_card_game/src/App.jsx
@@ -49,7 +49,8 @@ export default function App(){
 
   const [result, setResult] = useState('');
   const [cards, setCards] = useState(dealCards);
-  
+  const [gameState, setGameState] = useState('play');
+
   function compareCards(){
     const playerStat = cards.player[0].stats[0];
     const opponentStat = cards.opponent[0].stats[0];
@@ -62,7 +63,23 @@ export default function App(){
     }else{
       setResult("Loss");
     }
+    setGameState("result");
+  }
 
+  function nextRound(){
+    setCards(cards =>{
+      const playedCards = [{...cards.player[0]}, {...cards.opponent[0]}];
+      const player = cards.player.slice(1);
+      const opponent = cards.opponent.slice(1);
+      if(result === "Draw"){
+        return{
+          player,
+          opponent
+        };
+      }
+    });
+    setGameState('play');
+    setResult('');
   }
 
   return(
@@ -80,7 +97,13 @@ export default function App(){
         
         <div className='center-area'>
           <p>{result || 'Press the button'}</p>
-          <PlayButton text={'Play'} handleClick={compareCards} />
+          {
+            gameState === 'play' ?
+            ( <PlayButton text={'Play'} handleClick={compareCards} />)
+            :
+            ( <PlayButton text={"Next"} handleClick={nextRound} />)
+          }
+         
         </div>
 
         <ul className='card-list opponent'>
